dd7c97f45e125b856d24aea731dea78f7d3681b3
Manuel <manuel@guerrerocabrera.com>
2024-05-30 15:07:55 +0200

    30.05.2024: Modificando variable BASE_URL

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,11 +1,13 @@
+const BASE_URL = "http://localhost:3000/api/v1";
+
 // Variable (Objeto) que contiene las URLs de las peticiones a la API
 export const API_URLS = {
     // URL para fetch libros
-    books: "http://localhost:3000/api/v1/books",
+    books: `${BASE_URL}/books`,
     // URL para fetch users (Sirve para Register)
-    users: "http://localhost:3000/api/v1/users/",
+    users: `${BASE_URL}/users/`,
     // URL para añadir a favoritos
-    favorites: `http://localhost:3000/api/v1/users/${localStorage.getItem("userId")}`,
+    favorites: `${BASE_URL}/users/${localStorage.getItem("userId")}`,
     // URL para hacer login de usuario
-    login: "http://localhost:3000/api/v1/users/login",    
+    login: `${BASE_URL}/login`
 };
